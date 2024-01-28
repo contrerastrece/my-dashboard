a50f65cdfa15116f94f64f091d9769995fbf54e8
vcontreras <contrerastrece@gmail.com>
2024-01-28 18:44:45 -0500

    mantener favoritos

--- src/pokemons/components/PokemonGridFav.tsx
+++ src/pokemons/components/PokemonGridFav.tsx
@@ -1,20 +1,20 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { PokemonCard } from "..";
 import { usePokemonStore } from "@/store/pokemons/pokemons";
 
 export const PokemonGridFav = () => {
-  const pokeFav = usePokemonStore((state) => state.initialState);
-
-  console.log(Object.values(pokeFav));
-  const data = Object.values(pokeFav);
+  const pokeFav = usePokemonStore((state) => Object.values(state.initialState));
+  const [pokemons, setPokemons] = useState(pokeFav);
 
   return (
-    <div className="flex flex-wrap gap-10 items-center justify-center" >
-      {data.map((pokemon) => (
-        <PokemonCard key={pokemon.id} pokemon={pokemon} />
-      ))}
+    <div className="flex flex-wrap gap-10 items-center justify-center">
+      {pokemons.length > 0
+        ? pokemons.map((pokemon) => (
+            <PokemonCard key={pokemon.id} pokemon={pokemon} />
+          ))
+        : "No hay favoritos"}
     </div>
   );
 };
